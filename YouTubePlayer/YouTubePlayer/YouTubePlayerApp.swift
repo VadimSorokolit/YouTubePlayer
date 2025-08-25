3284530e249a10e5f142c2655d5e783b6c5e928b
VadimSorokolit <macintosh@ukr.net>
2025-08-25 10:17:30 +0300

    Init project

---
+++ YouTubePlayer/YouTubePlayer/YouTubePlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTubePlayerApp.swift
+//  YouTubePlayer
+//
+//  Created by Vadim Sorokolit on 25.08.2025.
+//
+    
+import SwiftUI
+
+@main
+struct YouTubePlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
