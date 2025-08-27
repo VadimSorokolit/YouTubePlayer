95c66d34d5865c6184167e904b63b974e06c2e9f
VadimSorokolit <macintosh@ukr.net>
2025-08-27 11:25:45 +0300

    implement struct model for Api key

---
+++ YouTubePlayer/YouTubePlayer/MVVM/Model/AppConfig.swift
@@ -0,0 +1,10 @@
+//
+//  AppConfig.swift
+//  YouTubePlayer
+//
+//  Created by Vadim Sorokolit on 27.08.2025.
+//
+    
+struct AppConfig: Decodable {
+    let apiKey: String
+}
---
+++ YouTubePlayer/YouTubePlayer/Resources/GlobalConstants.swift
@@ -0,0 +1,11 @@
+//
+//  GlobalConstants.swift
+//  YouTubePlayer
+//
+//  Created by Vadim Sorokolit on 27.08.2025.
+//
+    
+struct GlobalConstants {
+    static let apiKeyFileName = "ApiKey"
+    static let apiKeyFileExtension = "json"
+}
